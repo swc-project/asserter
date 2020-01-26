22d87810a18173ede471bf845c10f0ddbe4a4cb1
강동윤 <kdy1997.dev@gmail.com>
2020-01-26 11:11:25 +0900

    Fix and add documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,70 @@
 //! Assertion library for rust.
+//!
+//! In a function annotated wit assterer, you can use some macros.
+//!
+//! # `unwrap!(foo as Pat)`
+//!
+//! ```rust
+//! use asserter::*;
+//!
+//! fn main () {
+//!     let foo = Some("example");
+//!
+//!     // If there's no issue with rustfmt, use `as` like the code below.
+//!     unwrap!(foo as Some(s));
+//!     assert_eq!(s, "example");
+//! }
+//! ```
+//!
+//! Sometimes, rustfmt does not work for the default syntax. There's an
+//! alternative syntax to help rustfmt.
+//!
+//! ```rust
+//! use asserter::*;
+//! enum Complex {
+//!     Struct { foo: usize, bar: usize },
+//! }
+//! fn main () {
+//!     let foo = Complex::Struct { foo: 1, bar: 2 };
+//!
+//!     // This syntax allows formatting
+//!     unwrap!(foo, Complex::Struct { foo, bar });
+//! }
+//! ```
+//!
+//! Note that you **can** use box_patterns inside `unwrap!()` (on stable).
+//!
+//! ```rust
+//! enum Complex {
+//!     Normal(usize),
+//!     Boxed(Box<Complex>),
+//! }
+//! fn main () {
+//!     let foo = Complex::Boxed(Box::new(Complex::Normal(0)));
+//!
+//!     unwrap!(foo as box Complex::Boxed(Complex::Normal(v)));
+//!     assert_eq!(v, 0);
+//! }
+//! ```
+//!
+//!
+//! ## unbox!(Pat)
+//!
+//! If you want rustfmt to work correctly while unwrapping box, there's a helper
+//! for it.
+//!
+//! ```rust
+//! enum Complex {
+//!     Normal(usize),
+//!     Boxed(Box<Complex>),
+//! }
+//! fn main () {
+//!     let foo = Complex::Boxed(Box::new(Complex::Normal(0)));
+//!
+//!     unwrap!(foo as unbox!(Complex::Boxed(Complex::Normal(v))));
+//!     assert_eq!(v, 0);
+//! }
+//! ```
+//!  
 
 pub use asserter_macros::asserter;
--- tests/unwrap.rs
+++ tests/unwrap.rs
@@ -1,28 +1,30 @@
 use asserter::*;
-//
-//#[test]
-//#[asserter]
-//fn simple() {
-//    let foo = Some("f");
-//
-//    unwrap!(foo as Some(foo));
-//    assert_eq!(foo, "f");
-//}
-//
-//#[test]
-//#[should_panic]
-//#[asserter]
-//fn simple_panic() {
-//    let foo: Option<String> = None;
-//
-//    unwrap!(foo as Some(foo));
-//}
+
+#[test]
+#[asserter]
+fn simple() {
+    let foo = Some("f");
+
+    unwrap!(foo as Some(foo));
+    assert_eq!(foo, "f");
+}
+
+#[test]
+#[should_panic]
+#[asserter]
+fn simple_panic() {
+    let foo: Option<String> = None;
+
+    unwrap!(foo as Some(foo));
+}
 
 enum Complex {
     Path,
     Normal(String),
     Boxed(Box<Complex>),
     Ref(&'static Complex),
+    Tuple(String, usize),
+    Struct { foo: usize, bar: usize },
 }
 
 #[test]
@@ -38,11 +40,10 @@ fn complex_boxed() {
 #[test]
 #[asserter]
 fn complex_ref() {
-    let foo = Complex::Ref(&Complex::Normal(String::from("foo")));
+    static COMPLEX: Complex = Complex::Path;
+    let foo = Complex::Ref(&COMPLEX);
 
-    unwrap!(foo as Complex::Ref(&Complex::Normal(s)));
-
-    assert_eq!(s, "foo");
+    unwrap!(foo as Complex::Ref(&Complex::Path));
 }
 
 #[test]
@@ -51,4 +52,22 @@ fn complex_path() {
     let foo = Complex::Path;
 
     unwrap!(foo as Complex::Path);
+}
+
+#[test]
+#[asserter]
+fn complex_tuple() {
+    let foo = Complex::Tuple(String::new(), 5);
+
+    unwrap!(foo as Complex::Tuple(s, v));
+    assert_eq!(s, "");
+    assert_eq!(v, 5);
+}
+
+#[test]
+#[asserter]
+fn complex_struct() {
+    let foo = Complex::Struct { foo: 1, bar: 2 };
+
+    unwrap!(foo, Complex::Struct { foo, bar });
 }
